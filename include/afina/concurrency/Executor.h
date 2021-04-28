7a84d4f308da777e13e0d5675c0722bde50bde2f
r-kasp <53397898+r-kasp@users.noreply.github.com>
2021-04-29 02:39:32 +0300

    Pool Thread the first entry

--- include/afina/concurrency/Executor.h
+++ include/afina/concurrency/Executor.h
@@ -1,6 +1,8 @@
 #ifndef AFINA_CONCURRENCY_EXECUTOR_H
 #define AFINA_CONCURRENCY_EXECUTOR_H
 
+#include <chrono>
+#include <iostream>
 #include <condition_variable>
 #include <functional>
 #include <memory>
@@ -28,8 +30,26 @@ class Executor {
         kStopped
     };
 
-    Executor(std::string name, int size);
-    ~Executor();
+    Executor(std::string name, int _low_watermark = 1, int _hight_watermark = 2,
+    		int _max_queue_size = 100, int _idle_time = 1000)
+    {
+    	low_watermark = _low_watermark;
+    	hight_watermark = _hight_watermark;
+    	max_queue_size = _max_queue_size;
+    	idle_time = _idle_time;
+    	cur_work = 0;
+    	cur_threads = 0;
+    	
+    	std::unique_lock<std::mutex> lock(mutex);
+    	for (int i = 0; i < low_watermark; i++)
+    	{
+    		cur_threads++;
+    		threads.push_back( std::thread([this] {return perform(this);} ) );
+    	}
+    	state = State::kRun;
+    }
+    
+    ~Executor() { Stop(true); }
 
     /**
      * Signal thread pool to stop, it will stop accepting new jobs and close threads just after each become
@@ -37,7 +57,22 @@ class Executor {
      *
      * In case if await flag is true, call won't return until all background jobs are done and all threads are stopped
      */
-    void Stop(bool await = false);
+    void Stop(bool await = false)
+    {
+    	std::unique_lock<std::mutex> lock(mutex);
+    	if (state != State::kStopped)
+    	{
+    		if (cur_work == 0)
+				state = State::kStopped;
+			else
+    			state = State::kStopping;
+			empty_condition.notify_all();
+			while (state != State::kStopped)
+			{
+				stop_condition.wait(lock);
+			}
+		}
+    }
 
     /**
      * Add function to be executed on the threadpool. Method returns true in case if task has been placed
@@ -56,12 +91,27 @@ class Executor {
         }
 
         // Enqueue new task
+        
+        if (threads.size() < hight_watermark && threads.size() == cur_work)
+        {
+        	threads.push_back( std::thread([this] {return perform(this);} ) );
+        	cur_threads++;
+		}
+        
         tasks.push_back(exec);
         empty_condition.notify_one();
         return true;
     }
 
 private:
+	int size = 0;
+	int low_watermark = 0;
+	int hight_watermark = 0;
+	int max_queue_size = 0;
+	int idle_time = 0;
+	int cur_work = 0;
+	int cur_threads = 0;
+
     // No copy/move/assign allowed
     Executor(const Executor &);            // = delete;
     Executor(Executor &&);                 // = delete;
@@ -71,7 +121,52 @@ private:
     /**
      * Main function that all pool threads are running. It polls internal task queue and execute tasks
      */
-    friend void perform(Executor *executor);
+    friend void perform(Executor *executor)
+    {
+    	std::unique_lock<std::mutex> lock(executor->mutex);
+    	bool flag_time = false;
+    	while (executor->state == Executor::State::kRun || !executor->tasks.empty())
+    	{    	
+    		auto time = std::chrono::steady_clock::now();
+    		while (executor->tasks.empty())
+			{
+				auto wake_up = executor->empty_condition.wait_until(lock, time + std::chrono::milliseconds(executor->idle_time));
+				if (executor->state != Executor::State::kRun || 
+				(executor->threads.size() > executor->low_watermark && executor->tasks.empty() && wake_up == std::cv_status::timeout))
+				{
+					flag_time = true;
+					break;
+				}
+			}		
+			if (flag_time)
+				break;
+				
+			auto task = executor->tasks.front();
+			executor->tasks.pop_front();
+			executor->cur_work++;
+			
+			lock.unlock();
+			
+			try
+			{
+				task();
+			}
+			catch (...)
+			{
+				std::cout << "Error" << std::endl;
+			}
+			
+			lock.lock();
+			executor->cur_work--;
+    	}
+    	
+    	executor->cur_threads--;
+		if (executor->cur_threads == 0 && executor->state != Executor::State::kRun)
+		{
+			executor->state = Executor::State::kStopped;
+			executor->stop_condition.notify_all();
+		}
+    }
 
     /**
      * Mutex to protect state below from concurrent modification
@@ -82,6 +177,8 @@ private:
      * Conditional variable to await new data in case of empty queue
      */
     std::condition_variable empty_condition;
+    
+    std::condition_variable stop_condition;
 
     /**
      * Vector of actual threads that perorm execution
